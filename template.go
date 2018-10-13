af1d0c1a22f7b779cb8b2aced1ab03c575d5c721
Sam Ban <sam@gpmd.co.uk>
2018-10-13 19:32:03 +0100

    fallback RFC3339 for date function

--- template.go
+++ template.go
@@ -93,7 +93,10 @@ func dateFmt(format, datestring string) string {
 	layout := "2006-01-02 15:04:05"
 	t, err := time.Parse(layout, datestring)
 	if err != nil {
-		return datestring
+		t, err = time.Parse(time.RFC3339, datestring)
+		if err != nil {
+			return datestring
+		}
 	}
 	return t.Format(format)
 }
